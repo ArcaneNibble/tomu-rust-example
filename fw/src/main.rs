da302bb1ce87191d602fd041016904d6cb68cd92
Robert Ou <rqou@robertou.com>
2018-03-29 05:07:13 -0700

    Hook up RTFM

--- fw/src/main.rs
+++ fw/src/main.rs
@@ -1,7 +1,7 @@
 #![feature(alloc)]
 #![feature(global_allocator)]
 #![feature(lang_items)]
-#![feature(used)]
+#![feature(proc_macro)]
 #![no_std]
 
 extern crate alloc_cortex_m;
@@ -9,9 +9,11 @@ extern crate alloc_cortex_m;
 extern crate alloc;
 extern crate cortex_m;
 extern crate cortex_m_rt;
+extern crate cortex_m_rtfm as rtfm;
+extern crate efm32hg309;
 
 use alloc_cortex_m::CortexMHeap;
-use cortex_m::asm;
+use rtfm::{app};
 
 #[global_allocator]
 static ALLOCATOR: CortexMHeap = CortexMHeap::empty();
@@ -21,23 +23,17 @@ extern "C" {
     static mut _eheap: u32;
 }
 
-fn main() {
+app! {
+    device: efm32hg309,
+}
+
+fn init(p: init::Peripherals) {
     // Initialize the allocator
     let start = unsafe { &mut _sheap as *mut u32 as usize };
     let end = unsafe { &mut _eheap as *mut u32 as usize };
     unsafe { ALLOCATOR.init(start, end - start) }
-
-    // Test allocator
-    let xs = vec![0, 1, 2];
-
-    loop {}
 }
 
-// As we are not using interrupts, we just register a dummy catch all handler
-#[link_section = ".vector_table.interrupts"]
-#[used]
-static INTERRUPTS: [extern "C" fn(); 240] = [default_handler; 240];
-
-extern "C" fn default_handler() {
-    asm::bkpt();
+fn idle() -> ! {
+    loop { }
 }
