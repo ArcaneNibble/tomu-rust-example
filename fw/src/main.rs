cbd7c24d64b349ad8328049b3b7905eca29a154f
Robert Ou <rqou@robertou.com>
2018-03-29 05:03:41 -0700

    Enable allocator

--- fw/src/main.rs
+++ fw/src/main.rs
@@ -1,12 +1,35 @@
+#![feature(alloc)]
+#![feature(global_allocator)]
+#![feature(lang_items)]
 #![feature(used)]
 #![no_std]
 
+extern crate alloc_cortex_m;
+#[macro_use]
+extern crate alloc;
 extern crate cortex_m;
 extern crate cortex_m_rt;
 
+use alloc_cortex_m::CortexMHeap;
 use cortex_m::asm;
 
+#[global_allocator]
+static ALLOCATOR: CortexMHeap = CortexMHeap::empty();
+
+extern "C" {
+    static mut _sheap: u32;
+    static mut _eheap: u32;
+}
+
 fn main() {
+    // Initialize the allocator
+    let start = unsafe { &mut _sheap as *mut u32 as usize };
+    let end = unsafe { &mut _eheap as *mut u32 as usize };
+    unsafe { ALLOCATOR.init(start, end - start) }
+
+    // Test allocator
+    let xs = vec![0, 1, 2];
+
     loop {}
 }
 
